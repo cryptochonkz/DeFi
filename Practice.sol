69f41a879457aed213e0883361ba411c443ad6dc
TARISO <toni.ariso@gmail.com>
2021-02-22 08:39:05 +0100

    escrow part 1

--- Practice.sol
+++ Practice.sol
@@ -1,30 +1,34 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.2;
 
-contract Deed {
-    address public lawyer;
-    address payable public beneficiary;
-    uint public earliest;
-    uint public paidPayouts;
-    uint constant public PAYOUTS = 10;
-    uint constant public INTERVAL = 10;
-    
-    constructor(
-        address _lawyer,
-        address payable _beneficiary,
-        uint fromNow)
-        payable
-        public {
-            lawyer = _lawyer;
-            beneficiary = _beneficiary;
-            earliest = now + fromNow;
-            amount = msg.value / PAYOUTS;
-        }
-        
-    function withdraw() public {
-        require(msg.sender == beneficiary, 'lawyer only');
-        require(now >= earliest, "too early");
-        require(paidPayouts < PAYOUTS);
-        uint elligiblePayouts = (now - earliest) / INTERVAL;
-        beneficiary.transfer(elligiblePayouts * amount);
-    }
+contract Escrow {
+  address public payer;
+  address payable public payee;
+  address public lawyer;
+  uint public amount;
+
+  constructor(
+    address _payer;
+    address payable _payee;
+    uint _amount;
+  ) payable public {
+    msg.sender = lawyer;
+    payer = _payer;
+    payee = _payee;
+    amount = _amount;
+  }
+
+  function deposit() payable public{
+    require(msg.sender == payer, "must be payer");
+    require(address(this).balance <= amount, "must be equal or superior de amount");
+  }
+
+  function release() public {
+    require(msg.sender == lawyer, "must be lawyer");
+    require(address(this).balance == amount, "cannot be released");
+    payee.transfer(amount);
+      }
+
+  function balanceOf() view public returns(uint){
+    return address(this).balance;
+  }
 }
