a3b986bd810f20f7ab9f7b0aecfd9af39556329d
TARISO <toni.ariso@gmail.com>
2021-02-22 13:32:07 +0100

    MultiSig Part 2

--- Practice.sol
+++ Practice.sol
@@ -1,35 +1,70 @@
 pragma solidity ^0.5.2;
 
-contract Escrow {
-  address public lawyer;
-  uint public amount;
-  address payable public payer;
-  address public payee;
+contract MultiSig {
+  address[] public approvers;
+  uint public quorum;
+
+  struct Transfer {
+    uint id;
+    uint amount;
+    address payable to;
+    uint approvals;
+    bool sent
+  }
+
+  mapping(nextId => Transfer) transfers;
+  uint public nextId;
+  mapping(address => mapping(uint => bool)) approvals;
 
   constructor(
-    address payable _payer;
-    address _payee;
-    uint amount;
-  ) public {
-    msg.sender = lawyer;
-    amount = _amount;
-    payer = _payer;
-    payee = _payee;
+    address[] memory _approvers,
+    uint _quorum
+  ) 
+  payable public
+  {
+    approvers = _approvers;
+    quorum = _quorum;
+
   }
 
-  function deposit() payable public {
-    require(msg.sender == payer, "must be payer");
-    require(address(this).balance <= amount);
-  }
+  function createTransfer(uint amount, address payable to) 
+    external  onlyApprover()
+    {
+      transfers[nextId] = Transfer {
+        nextId,
+        amount,
+        to,
+        0,
+        false
+      };
+      nextId++;
+   }
 
-  function withdraw() public {
-    require(msg.sender == lawyer, "must be lawyer");
-    require(address(this).balance == amount, "must be balance")
-    payee.transfer(amount);
-  }
+   function sendTransfer(uint id) external onlyApprover(){
+      require(transfers[id].sent == false, "The transfer has been sent");
+      if(approvals[msg.sender][id] == false) {
+        approvals[msg.sender][id] = true;
+        transfers[id].approvals++;
+      }
 
+      if(transfers[id].approvals >= quorum) {
+        uint amount = transfers[id].amount;
+        address payable to = transfers[id].to;
+        to.transfer(amount);
+        transfers[id].sent = true;
+        return;
+      }
+   }
 
-  funciton balanceOf() view public returns(uint){
-    return address(this).balance;
-  }
+   modifier onlyApprover(){
+     bool allowed = false;
+     for(uint i = 0; i < approvers.length; i++) {
+       if(approvers[i] == msg.sender) {
+         allowed = true;
+       }
+     }
+
+     require(allowed == true, "Not allowed");
+     _;
+   }
 }
